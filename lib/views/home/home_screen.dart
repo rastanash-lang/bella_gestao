5f7ecb700da13ca12d400395ec420cd97d17f0bf
rastanash-lang <rastanash@gmail.com>
2026-08-27 12:01:44 -0300

    Update home_screen.dart

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,1 +1,37 @@
+import 'package:flutter/material.dart';
+import '../dashboard/dashboard_view.dart';
+import '../relatorios/relatorios_view.dart';
+import '../relatorios/painel_mei_view.dart';
 
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _indiceAtual = 0;
+
+  final List<Widget> _telas = const [
+    DashboardView(),
+    RelatoriosView(),
+    PainelMeiView(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _telas[_indiceAtual],
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _indiceAtual,
+        onDestinationSelected: (idx) => setState(() => _indiceAtual = idx),
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.account_balance_wallet_outlined), selectedIcon: Icon(Icons.account_balance_wallet), label: 'Caixa'),
+          NavigationDestination(icon: Icon(Icons.analytics_outlined), selectedIcon: Icon(Icons.analytics), label: 'Relatórios'),
+          NavigationDestination(icon: Icon(Icons.badge_outlined), selectedIcon: Icon(Icons.badge), label: 'Painel MEI'),
+        ],
+      ),
+    );
+  }
+}
