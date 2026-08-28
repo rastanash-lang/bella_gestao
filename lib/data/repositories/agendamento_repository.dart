2934f5358b34941c668a85a8ecc7fe6ca1b84c68
rastanash-lang <rastanash@gmail.com>
2026-08-28 18:01:45 -0300

    Update agendamento_repository.dart

--- lib/data/repositories/agendamento_repository.dart
+++ lib/data/repositories/agendamento_repository.dart
@@ -1,1 +1,37 @@
+import 'package:sqflite/sqflite.dart';
+import '../../core/database/db_helper.dart';
+import '../models/agendamento_model.dart';
 
+class AgendamentoRepository {
+  final DatabaseHelper dbHelper = DatabaseHelper.instance;
+
+  Future<int> inserir(Agendamento agendamento) async {
+    final db = await dbHelper.database;
+    return await db.insert('agendamentos', agendamento.toMap());
+  }
+
+  Future<List<Agendamento>> listarTodos() async {
+    final db = await dbHelper.database;
+    final result = await db.query('agendamentos', orderBy: 'dataHoraInicio ASC');
+    return result.map((json) => Agendamento.fromMap(json)).toList();
+  }
+
+  Future<int> atualizarStatus(int id, String novoStatus) async {
+    final db = await dbHelper.database;
+    return await db.update(
+      'agendamentos',
+      {'status': novoStatus},
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future<int> deletar(int id) async {
+    final db = await dbHelper.database;
+    return await db.delete(
+      'agendamentos',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}
